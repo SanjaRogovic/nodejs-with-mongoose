8066cab1251aaabd09e42f87e83c6831149f8a0b
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-10-07 10:14:29 +0200

    created student schema

---
+++ models/Student.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const StudentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    first_name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    }
+})
+
+const Student = mongoose.model("Student", StudentSchema)
+
+export default Student
