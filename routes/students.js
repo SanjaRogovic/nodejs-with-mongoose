b173d706262240cd4192547ca8b7f6c561fa6381
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-10-07 11:10:55 +0200

    edit one student by id

--- routes/students.js
+++ routes/students.js
@@ -4,7 +4,12 @@ import Student from "../models/Student.js";
 const studentsRouter = express.Router()
 
 
-// Create new student
+// CREATE A NEW STUDENT
+
+/*If the user sends a POST request, create a database document with:
+name: “John”
+first_name: “Doe”
+email: “john@doe.com“*/
 
 studentsRouter.post("/", async (req, res) => {
     try  {
@@ -17,7 +22,7 @@ studentsRouter.post("/", async (req, res) => {
 })
 
 
-// Retrieve all students
+// RETRIEVE ALL STUDENTS
 
 studentsRouter.get("/", async (req, res) => {
     try  {
@@ -29,7 +34,7 @@ studentsRouter.get("/", async (req, res) => {
 })
 
 
-// Retrieve one student by id
+// RETRIEVE ONE STUDENT BY ID
 
 studentsRouter.get("/:id", async (req, res) => {
     try  {
@@ -42,13 +47,16 @@ studentsRouter.get("/:id", async (req, res) => {
 })
 
 
-// Edit a student
+// EDIT A STUDENT
 
 studentsRouter.put("/:id", async (req, res) => {
     try  {
         const {id} = req.params
         const {name, first_name, email} = req.body
         const result = await Student.findByIdAndUpdate(id, {name, first_name, email})
+        if(result.length === 0){
+            res.status(404).json({message: "User not found"})
+        }
         res.json(result)
     } catch (error) {
         res.status(500).res(error)
