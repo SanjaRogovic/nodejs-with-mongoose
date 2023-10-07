c6ebd9e65401f56c92fd4ae660402456f11a3a26
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-10-07 11:03:23 +0200

    installed express validator

--- routes/students.js
+++ routes/students.js
@@ -41,4 +41,18 @@ studentsRouter.get("/:id", async (req, res) => {
     }
 })
 
+
+// Edit a student
+
+studentsRouter.put("/:id", async (req, res) => {
+    try  {
+        const {id} = req.params
+        const {name, first_name, email} = req.body
+        const result = await Student.findByIdAndUpdate(id, {name, first_name, email})
+        res.json(result)
+    } catch (error) {
+        res.status(500).res(error)
+    }
+})
+
 export default studentsRouter
