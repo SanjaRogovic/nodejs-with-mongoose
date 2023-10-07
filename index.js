499f54b119dd3aec515254e404dca6e07a02badc
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-10-07 09:46:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express  from "express"
+
+const app = express()
+
+
+const port = process.env.PORT || 3000
+
+app.listen("port", (req, res) => {
+    console.log(`Server listening on port ${port}`)
+})
+
